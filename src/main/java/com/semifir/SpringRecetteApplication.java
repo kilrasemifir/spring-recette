767e650bbdb086e521fca26b9470a16fc3260912
Raoux <killian@semifir.com>
2020-04-03 00:02:24 +0200

    first commit

---
+++ src/main/java/com/semifir/SpringRecetteApplication.java
@@ -0,0 +1,13 @@
+package com.semifir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecetteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRecetteApplication.class, args);
+	}
+
+}
